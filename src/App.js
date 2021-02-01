669f30720db76f02ab4ae5d651df4154cefacb70
ZQ M <zqm@ZQs-MacBook-Pro.fios-router.home>
2021-02-01 17:22:07 -0500

    updated menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component.jsx';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
+const Hats = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop/hats' component={Hats} />
+      </Switch>
     </div>
   );
 }
