f4682f12fe8a57c85372a925d5a4a5816357aa17
ZQ M <zqm@ZQs-MacBook-Pro.fios-router.home>
2021-02-02 00:00:42 -0500

    updated app with a shop page, collection review component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component.jsx';
+import ShopPage from './pages/shop-page/shop-page.component.jsx';
 import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
-const Hats = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop/hats' component={Hats} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
