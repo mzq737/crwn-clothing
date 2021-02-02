d753f40296eba629a790de3f69826f0377fe6d2a
ZQ M <zqm@ZQs-MacBook-Pro.fios-router.home>
2021-02-02 12:57:48 -0500

    added header component and links to home, shop page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop-page/shop-page.component.jsx';
+import Header from './components/header/header.component.jsx';
 import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
@@ -8,6 +9,7 @@ import './App.css';
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
