17581052f033aee5403a4075aa84b1b52a84880b
ZQ M <zqm@ZQs-MacBook-Pro.fios-router.home>
2021-02-13 07:24:35 -0500

    updating stripe integration and updating our favicon and title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItemsTotal, selectCartItems } from '../../redux/cart/cart.selectors';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import './checkout.styles.scss';
 
@@ -31,6 +32,12 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className='total'>
       <span>Total: ${total} </span>
     </div>
+    <div className='test-warning'>
+      *Please use the following tset credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 01/23 - CVV: 123 
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 )
 
